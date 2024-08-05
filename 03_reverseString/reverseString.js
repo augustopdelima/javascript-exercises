ce939bfb8deecca7961c05fe515eefc949624333
Augusto Pasquali de Lima <augusto.pasqualidelima@gmail.com>
2024-08-05 15:27:49 -0300

    Resolve javascript exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+  let reverseString = '';
+  let wordLength = word.length;
+  for (let i = wordLength; i > 0; i--) {
+    reverseString += word.at(i - 1);
+  }
+
+  return reverseString
 
 };
 
