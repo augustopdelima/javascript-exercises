79ef85b6d18da30f3298507f74411ec69e81dac4
Augusto Pasquali de Lima <augusto.pasqualidelima@gmail.com>
2024-08-05 16:43:31 -0300

    Resolve javascript exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  const divisableBy4 = year % 4 === 0;
+  const divisableBy100 = year % 100 === 0;
+  const divisableBy400 = year % 400 === 0;
 
+  const leapYear = divisableBy4 && (divisableBy400 || !divisableBy100);
+  return leapYear;
 };
 
 // Do not edit below this line
