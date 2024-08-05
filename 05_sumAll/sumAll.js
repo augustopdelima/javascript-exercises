cf29bb8bcdc26f32eed4f79c319c05347c67d81c
Augusto Pasquali de Lima <augusto.pasqualidelima@gmail.com>
2024-08-05 16:11:55 -0300

    Resolve javascript exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+  if (!Number.isInteger(first) || !Number.isInteger(second)) return 'ERROR';
 
+  const firstNumber = Number(first);
+  const secondNumber = Number(second);
+
+  if (firstNumber < 0 || secondNumber < 0) return 'ERROR';
+
+
+  const bigger = firstNumber > secondNumber ? firstNumber : secondNumber;
+
+  const sum = bigger * ((firstNumber + secondNumber) / 2);
+
+  return sum;
 };
 
 // Do not edit below this line
