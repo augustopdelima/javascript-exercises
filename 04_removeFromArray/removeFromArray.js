d382849ec086e2cb0828b6ebf0670f0e573bed85
Augusto Pasquali de Lima <augusto.pasqualidelima@gmail.com>
2024-08-05 15:38:39 -0300

    Resolve javascript exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToRemove) {
+
+  return array.filter((item) => !elementsToRemove.includes(item));
+
 };
 
 // Do not edit below this line
